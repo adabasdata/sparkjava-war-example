77d06f133a6f7fe214c2b3ea96ee6a2562743cd0
adabasdata <146067277+adabasdata@users.noreply.github.com>
2026-02-03 04:51:53 -0800

    error resolved

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 import spark.servlet.SparkApplication;
 
-import static spark.Spark.g
+import static spark.Spark.get;
 
 public class HelloWorld implements SparkApplication {
 	public static void main(String[] args) {
